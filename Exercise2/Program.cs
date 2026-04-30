943bf8f1dd92fbc1f86c674b28aae45276d3aa2f
Martin Leo <martin.christoffer.leo@gmail.com>
2026-04-30 14:09:14 +0200

    case 1, price checker based on age, implemented

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -8,9 +8,9 @@
 
             do
             {
-                Console.WriteLine("\n*******************************");
+                Console.WriteLine("\n*********************************");
                 Console.WriteLine("Use the numbers to choose an option below.");
-                Console.WriteLine("1. Choice number 1.");
+                Console.WriteLine("1. Get price based on your age.");
                 Console.WriteLine("2. Choice number 2.");
                 Console.WriteLine("3. Choice number 3.");
                 Console.WriteLine("0. Exit.");
@@ -19,34 +19,81 @@
                 Console.Write("\n> ");
 
                 int input;
-                bool parseSuccess = int.TryParse(Console.ReadLine(), out input);
+                bool mainMenuChoiceParseSuccess = int.TryParse(Console.ReadLine(), out input);
 
-                if (parseSuccess)
+                if (mainMenuChoiceParseSuccess)
                 {
                     switch (input)
                     {
                         case 0:
-                            Console.WriteLine("Exiting, see you next time!");
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.WriteLine("\nExiting, see you next time!");
+                            Console.ResetColor();
                             running = false;
                             break;
                         case 1:
-                            Console.WriteLine("Menu option 1 takes your here.");
+                            Console.WriteLine("\n*********************************");
+                            Console.WriteLine("Pricing by age");
+                            Console.WriteLine("Enter your age below: ");
+                            Console.Write("> ");
+
+                            int ageInput;
+                            bool ageParseSuccess = int.TryParse(Console.ReadLine(), out ageInput);
+
+                            if(ageParseSuccess)
+                            {
+                                if(ageInput < 20)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Cyan;
+                                    Console.WriteLine("Ungdomspris: 80kr");
+                                    Console.ResetColor();
+                                } else if (ageInput > 64)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Cyan;
+                                    Console.WriteLine("Pensionärspris: 90kr");
+                                    Console.ResetColor();
+                                } else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Cyan;
+                                    Console.WriteLine("Standardpris: 120kr");
+                                    Console.ResetColor();
+                                }
+                            } 
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("\nCould not understand the command, try again using only numbers!");
+                                Console.ResetColor();
+                            }
+
+
+                            Console.WriteLine("*********************************");
                             break;
                         case 2:
+                            Console.WriteLine("\n*********************************");
+                            Console.ForegroundColor = ConsoleColor.Magenta;
                             Console.WriteLine("Menu option 2 takes your here.");
+                            Console.ResetColor();
                             break;
                         case 3:
+                            Console.WriteLine("\n*********************************");
+                            Console.ForegroundColor = ConsoleColor.Magenta;
                             Console.WriteLine("Menu option 3 takes your here.");
+                            Console.ResetColor();
                             break;
                         default:
-                            Console.WriteLine("No command matches that input, try agan!");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nNo command matches that input, try agan!");
+                            Console.ResetColor();
                             break;
 
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Could not understand the command, try again, use only numbers!");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nCould not understand the command, try again using only numbers!");
+                    Console.ResetColor();
                 }
 
                
